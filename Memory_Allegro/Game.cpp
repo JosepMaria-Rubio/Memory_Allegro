86f6748fac28f284c6ab4269c86b8c8ee36c3d5d
JosepMaria-Rubio <josepmariarubiosala@gmail.com>
2025-01-16 00:24:46 +0100

    Final Release

--- Memory_Allegro/Game.cpp
+++ Memory_Allegro/Game.cpp
@@ -48,12 +48,12 @@ void Game::StartGame(ALLEGRO_DISPLAY* ventana) {
     int botonesGame[] = { 0, 0, 0, 0 };
     int botonExit = 0;
     turns = 0;
-    bool running = true;
-    bool time = true;
+    bool isRunning = true;
+    bool isPaused = true;
 	attemps = 0;
     sec = 0;
     al_start_timer(segundoTimer);
-    while (running) {
+    while (isRunning) {
         ALLEGRO_EVENT Evento;
         al_wait_for_event(event_queue, &Evento);
         al_draw_bitmap(background, 0, 0, 0);
@@ -70,47 +70,45 @@ void Game::StartGame(ALLEGRO_DISPLAY* ventana) {
         al_draw_text(arial35, al_map_rgb(0, 0, 0), 1080, 55, NULL, (to_string(sec)).c_str());
 
         if (Evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-            running = false;
+            isRunning = false;
         }
 
         if (Evento.type == ALLEGRO_EVENT_MOUSE_AXES) {
             botonesGame[0] = (x >= 375 && x <= 575 && y >= 50 && y <= 100) ? 1 : 0; 
             botonesGame[1] = (x >= 625 && x <= 825 && y >= 50 && y <= 100) ? 1 : 0;
         }
-        if(!time) al_draw_bitmap(continue1, 625, 50, 0);
+        if(!isPaused) al_draw_bitmap(continue1, 625, 50, 0);
 
         if (botonesGame[0]) al_draw_bitmap(surrenderHover, 375, 50, 0);
         if (botonesGame[1]) {
-                if(time) al_draw_bitmap(pauseHover, 625, 50, 0);
+                if(isPaused) al_draw_bitmap(pauseHover, 625, 50, 0);
                 else al_draw_bitmap(continue2, 625, 50, 0);
         }
-            
-       
-            
+
 
         if (Evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
             if (Evento.mouse.button & 1) {
                 if (botonesGame[0] == 1) {
-                    running = false;
+                    isRunning = false;
                 }
                 if (botonesGame[1] == 1) {
-                    if (time) {
+                    if (isPaused) {
                         al_stop_timer(segundoTimer);
-                        time = false;
+                        isPaused = false;
                     }
                     else {
                         al_start_timer(segundoTimer);
-                        time = true;
+                        isPaused = true;
                     }
                 }
             }
         }
 
-        if (time) {
+        if (isPaused) {
             if (Evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
                 for (int i = 0; i < cards.size(); i++) {
                     if (x >= cards[i].getPositionTop()[0] && x <= cards[i].getPositionBottom()[0] && y >= cards[i].getPositionTop()[1] && y <= cards[i].getPositionBottom()[1]) {
-                        if (Evento.mouse.button & 1 && flippedCards.size() < 2) {
+                        if (Evento.mouse.button & 1 && flippedCards.size() < 2 && !cards[i].IsFlipped()) {
                             cards[i].Flip();
                             flippedCards.push_back(&cards[i]);
                             timReset = 0;
@@ -146,7 +144,7 @@ void Game::StartGame(ALLEGRO_DISPLAY* ventana) {
                         al_start_timer(segundoTimer); // Reinicia el temporizador
                     }
                     if (botonesGame[3] == 1) {
-                        running = false;
+                        isRunning = false;
                     }
                 }
             }
@@ -154,7 +152,7 @@ void Game::StartGame(ALLEGRO_DISPLAY* ventana) {
 
         if (Evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
             if (Evento.mouse.button & 1) {
-                if (botonExit) running = false;
+                if (botonExit) isRunning = false;
             }
         }
         al_flip_display();
@@ -170,10 +168,11 @@ void Game::StartGame(ALLEGRO_DISPLAY* ventana) {
     al_destroy_bitmap(continue2);
     al_destroy_bitmap(youWin);
 
+    
+    al_destroy_sample(wrongAnswer);
+    al_destroy_sample(correctAnswer);
+    al_destroy_sample(victoryMusic);
     al_uninstall_audio();
-    //al_destroy_sample(wrongAnswer);
-    //al_destroy_sample(correctAnswer);
-    //al_destroy_sample(victoryMusic);
     al_destroy_font(arial70);
     al_destroy_font(arial35);
     al_destroy_event_queue(event_queue);
@@ -188,12 +187,14 @@ void Game::StartGame(ALLEGRO_DISPLAY* ventana) {
 
 void Game::LoadCards() {
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    auto rng = std::default_random_engine{ seed };
     SetCardNames();
+	std::shuffle(cardNames.begin(), cardNames.end(), rng);
     for (int i = 0; i < numPairs; i++) {
         cards.push_back(Card("data/img/Cartas/" + cardNames[i] + ".png", cardNames[i]));
         cards.push_back(Card("data/img/Cartas/" + cardNames[i] + ".png", cardNames[i]));
     }
-	auto rng = std::default_random_engine{seed};
+	
 	std::shuffle(cards.begin(), cards.end(), rng);
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 6; j++) {
